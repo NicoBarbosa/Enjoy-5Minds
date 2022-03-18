f17747e159810be951aaa81744774c1c9028ab21
Nicolas Penante <66042022+NicoBarbosa@users.noreply.github.com>
2022-03-18 15:26:21 -0300

    First Commit

---
+++ enjoy-orm/src/main/java/br/com/fiap/enjoy/teste/TesteEnjoy.java
@@ -0,0 +1,10 @@
+package br.com.fiap.enjoy.teste;
+
+import javax.persistence.Persistence;
+
+public class TesteEnjoy {
+
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("enjoy-orm").createEntityManager();
+	}
+}

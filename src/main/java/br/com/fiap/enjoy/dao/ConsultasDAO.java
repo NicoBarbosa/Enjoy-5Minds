daec5a801f4fb5ee854e1621e31bf0c89b76a95b
Nicolas Penante <nicolas.penante10@hotmail.com>
2022-03-24 21:47:27 -0300

    getFrequenciaVisita em processo

--- src/main/java/br/com/fiap/enjoy/dao/ConsultasDAO.java
+++ src/main/java/br/com/fiap/enjoy/dao/ConsultasDAO.java
@@ -1,5 +1,6 @@
 package br.com.fiap.enjoy.dao;
 
+import javax.management.Query;
 import javax.persistence.EntityManager;
 
 public class ConsultasDAO {
@@ -7,5 +8,12 @@ public class ConsultasDAO {
 	
 	public ConsultasDAO(EntityManager em) {
 		this.em = em;
+	}
+	
+	public void getFrequenciaVisitas(int numTelefone) {
+		Query query = (Query) em.createQuery(
+				"ROUND(COUNT(DISTINCT vis.dt_visita) / COUNT(DISTINCT TO_CHAR(vis.dt_visita, 'mm/YY')),0)\n"
+			  + "from tb_enjoy_visita vis JOIN tb_enjoy_consumidor con"
+				);
 	}
 }

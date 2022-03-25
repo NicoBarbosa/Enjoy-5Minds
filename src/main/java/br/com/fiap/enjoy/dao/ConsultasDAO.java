d9e113b5e82020fb8119dbbc8a1762a1bc138e6a
Amanda <Amanda@192.168.1.109>
2022-03-24 22:39:39 -0300

    ticket medio

--- src/main/java/br/com/fiap/enjoy/dao/ConsultasDAO.java
+++ src/main/java/br/com/fiap/enjoy/dao/ConsultasDAO.java
@@ -1,7 +1,7 @@
 package br.com.fiap.enjoy.dao;
 
+import javax.management.Query;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 public class ConsultasDAO {
 	private EntityManager em;
@@ -12,16 +12,33 @@ public class ConsultasDAO {
 	
 	@SuppressWarnings("unused")
 	private void getTicketMedio(int nr_telefone) {
-		Query query = em.createQuery(
+		Query query = (Query) em.createQuery(
 				"SELECT AVG(VL_CONSUMIDO) as valor_gasto"
 				+ "ROM TB_ENJOY_VISITA vis"
 				+ "inner join tb_enjoy_consumidor con"
 				+ "on con.id_consumidor = vis.id_consumidor"
 				+ "where con.nr_telefone = 987654321");
-		query.setParameter("tel", nr_telefone);
-		var result = query.getSingleResult();
+		((javax.persistence.Query) query).setParameter("tel", nr_telefone);
+		var result = ((javax.persistence.Query) query).getSingleResult();
 		System.out.println("Ticket médio: R$ " + result);
+	}
+
+	public void getFrequenciaVisitas(int numTelefone) {
+		Query query = (Query) em.createQuery(
+				"ROUND(COUNT(DISTINCT vis.dt_visita) / COUNT(DISTINCT TO_CHAR(vis.dt_visita, 'mm/YY')),0)\n"
+			  + "from tb_enjoy_visita vis JOIN tb_enjoy_consumidor con"
+				);
+	}
+
+	private void getTicketMedio() {
+		
 		
 	}
 	
+	private void getBebidaConsumida() {
+		
+		
+	}
+	
+
 }

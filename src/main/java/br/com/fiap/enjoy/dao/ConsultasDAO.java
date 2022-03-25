5564c7a706cc92cb0d941a35d3716bc9e5767f12
Amanda <Amanda@192.168.1.109>
2022-03-24 22:16:23 -0300

    ticket medio

--- src/main/java/br/com/fiap/enjoy/dao/ConsultasDAO.java
+++ src/main/java/br/com/fiap/enjoy/dao/ConsultasDAO.java
@@ -1,6 +1,7 @@
 package br.com.fiap.enjoy.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 public class ConsultasDAO {
 	private EntityManager em;
@@ -9,8 +10,17 @@ public class ConsultasDAO {
 		this.em = em;
 	}
 	
-	private void getTicketMedio {
-		
+	@SuppressWarnings("unused")
+	private void getTicketMedio(int nr_telefone) {
+		Query query = em.createQuery(
+				"SELECT AVG(VL_CONSUMIDO) as valor_gasto"
+				+ "ROM TB_ENJOY_VISITA vis"
+				+ "inner join tb_enjoy_consumidor con"
+				+ "on con.id_consumidor = vis.id_consumidor"
+				+ "where con.nr_telefone = 987654321");
+		query.setParameter("tel", nr_telefone);
+		var result = query.getSingleResult();
+		System.out.println("Ticket médio: R$ " + result);
 		
 	}
 	
